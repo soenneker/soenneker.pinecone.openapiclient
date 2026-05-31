233b2732adee7de561b5ee29fcb96a0f4ef82bdd
Jake Soenneker <jake@soenneker.com>
2026-05-31 17:52:06 +0000

    Automated update

--- src/Soenneker.Pinecone.OpenApiClient/Models/ModelInfoSupportedParameter.cs
+++ src/Soenneker.Pinecone.OpenApiClient/Models/ModelInfoSupportedParameter.cs
@@ -18,18 +18,18 @@ namespace Soenneker.Pinecone.OpenApiClient.Models
         /// <summary>The allowed parameter values when the type is &apos;one_of&apos;.</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public List<global::Soenneker.Pinecone.OpenApiClient.Models.UnionBranch>? AllowedValues { get; set; }
+        public List<string>? AllowedValues { get; set; }
 #nullable restore
 #else
-        public List<global::Soenneker.Pinecone.OpenApiClient.Models.UnionBranch> AllowedValues { get; set; }
+        public List<string> AllowedValues { get; set; }
 #endif
         /// <summary>The default value for the parameter when a parameter is optional.</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public global::Soenneker.Pinecone.OpenApiClient.Models.UnionBranch? Default { get; set; }
+        public global::Soenneker.Pinecone.OpenApiClient.Models.ModelInfoSupportedParameter.ModelInfoSupportedParameter_default? Default { get; set; }
 #nullable restore
 #else
-        public global::Soenneker.Pinecone.OpenApiClient.Models.UnionBranch Default { get; set; }
+        public global::Soenneker.Pinecone.OpenApiClient.Models.ModelInfoSupportedParameter.ModelInfoSupportedParameter_default Default { get; set; }
 #endif
         /// <summary>The maximum allowed value (inclusive) when the type is &apos;numeric_range&apos;.</summary>
         public double? Max { get; set; }
@@ -86,8 +86,8 @@ namespace Soenneker.Pinecone.OpenApiClient.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
-                { "allowed_values", n => { AllowedValues = n.GetCollectionOfObjectValues<global::Soenneker.Pinecone.OpenApiClient.Models.UnionBranch>(global::Soenneker.Pinecone.OpenApiClient.Models.UnionBranch.CreateFromDiscriminatorValue)?.AsList(); } },
-                { "default", n => { Default = n.GetObjectValue<global::Soenneker.Pinecone.OpenApiClient.Models.UnionBranch>(global::Soenneker.Pinecone.OpenApiClient.Models.UnionBranch.CreateFromDiscriminatorValue); } },
+                { "allowed_values", n => { AllowedValues = n.GetCollectionOfPrimitiveValues<string>()?.AsList(); } },
+                { "default", n => { Default = n.GetObjectValue<global::Soenneker.Pinecone.OpenApiClient.Models.ModelInfoSupportedParameter.ModelInfoSupportedParameter_default>(global::Soenneker.Pinecone.OpenApiClient.Models.ModelInfoSupportedParameter.ModelInfoSupportedParameter_default.CreateFromDiscriminatorValue); } },
                 { "max", n => { Max = n.GetDoubleValue(); } },
                 { "min", n => { Min = n.GetDoubleValue(); } },
                 { "parameter", n => { Parameter = n.GetStringValue(); } },
@@ -103,8 +103,8 @@ namespace Soenneker.Pinecone.OpenApiClient.Models
         public virtual void Serialize(ISerializationWriter writer)
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
-            writer.WriteCollectionOfObjectValues<global::Soenneker.Pinecone.OpenApiClient.Models.UnionBranch>("allowed_values", AllowedValues);
-            writer.WriteObjectValue<global::Soenneker.Pinecone.OpenApiClient.Models.UnionBranch>("default", Default);
+            writer.WriteCollectionOfPrimitiveValues<string>("allowed_values", AllowedValues);
+            writer.WriteObjectValue<global::Soenneker.Pinecone.OpenApiClient.Models.ModelInfoSupportedParameter.ModelInfoSupportedParameter_default>("default", Default);
             writer.WriteDoubleValue("max", Max);
             writer.WriteDoubleValue("min", Min);
             writer.WriteStringValue("parameter", Parameter);
@@ -112,6 +112,86 @@ namespace Soenneker.Pinecone.OpenApiClient.Models
             writer.WriteStringValue("type", Type);
             writer.WriteStringValue("value_type", ValueType);
             writer.WriteAdditionalData(AdditionalData);
+        }
+        /// <summary>
+        /// Composed type wrapper for classes <see cref="bool"/>, <see cref="float"/>, <see cref="int"/>, <see cref="string"/>
+        /// </summary>
+        [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
+        public partial class ModelInfoSupportedParameter_default : IComposedTypeWrapper, IParsable
+        {
+            /// <summary>Composed type representation for type <see cref="bool"/></summary>
+            public bool? Boolean { get; set; }
+            /// <summary>Composed type representation for type <see cref="float"/></summary>
+            public float? Float { get; set; }
+            /// <summary>Composed type representation for type <see cref="int"/></summary>
+            public int? Integer { get; set; }
+            /// <summary>Composed type representation for type <see cref="string"/></summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+            public string? String { get; set; }
+#nullable restore
+#else
+            public string String { get; set; }
+#endif
+            /// <summary>
+            /// Creates a new instance of the appropriate class based on discriminator value
+            /// </summary>
+            /// <returns>A <see cref="global::Soenneker.Pinecone.OpenApiClient.Models.ModelInfoSupportedParameter.ModelInfoSupportedParameter_default"/></returns>
+            /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
+            public static global::Soenneker.Pinecone.OpenApiClient.Models.ModelInfoSupportedParameter.ModelInfoSupportedParameter_default CreateFromDiscriminatorValue(IParseNode parseNode)
+            {
+                if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
+                var result = new global::Soenneker.Pinecone.OpenApiClient.Models.ModelInfoSupportedParameter.ModelInfoSupportedParameter_default();
+                if(parseNode.GetBoolValue() is bool booleanValue)
+                {
+                    result.Boolean = booleanValue;
+                }
+                else if(parseNode.GetFloatValue() is float floatValue)
+                {
+                    result.Float = floatValue;
+                }
+                else if(parseNode.GetIntValue() is int integerValue)
+                {
+                    result.Integer = integerValue;
+                }
+                else if(parseNode.GetStringValue() is string stringValue)
+                {
+                    result.String = stringValue;
+                }
+                return result;
+            }
+            /// <summary>
+            /// The deserialization information for the current model
+            /// </summary>
+            /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
+            public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
+            {
+                return new Dictionary<string, Action<IParseNode>>();
+            }
+            /// <summary>
+            /// Serializes information the current object
+            /// </summary>
+            /// <param name="writer">Serialization writer to use to serialize this model</param>
+            public virtual void Serialize(ISerializationWriter writer)
+            {
+                if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
+                if(Boolean != null)
+                {
+                    writer.WriteBoolValue(null, Boolean);
+                }
+                else if(Float != null)
+                {
+                    writer.WriteFloatValue(null, Float);
+                }
+                else if(Integer != null)
+                {
+                    writer.WriteIntValue(null, Integer);
+                }
+                else if(String != null)
+                {
+                    writer.WriteStringValue(null, String);
+                }
+            }
         }
     }
 }
